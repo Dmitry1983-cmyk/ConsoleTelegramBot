82f532ddab3aec5c4de1103a52681264b6f7b6e1
user-machine <user-machine@Barabash-machin>
2020-10-06 16:36:13 +0000

    qwe

---
+++ ConsoleTelegramBot/ConsoleTelegramBot/CurrencySender.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using System.Xml;
+
+namespace ConsoleTelegramBot
+{
+    class CurrencySender
+    {
+        static TelegramBotClient client = new TelegramBotClient("1357650416:AAHwSAl43BQDVz4fCPuTvjOi8rnyTB8NNAE");
+
+       Timer timer;
+      public  long telegramId;
+
+        public CurrencySender(long telegraID, int p)
+        {
+            this.telegramId = telegraID;
+            timer = new Timer(p);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            string url = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+            XmlTextReader streamReader = new XmlTextReader(url);
+            string tmp_currency = "";
+            while (streamReader.Read())
+            {
+                if (streamReader.HasAttributes)
+                {
+                    tmp_currency += (streamReader.GetAttribute("ccy") + "  | " + streamReader.GetAttribute("base_ccy") + " Продажа : " + streamReader.GetAttribute("buy") + " Покупка : " + streamReader.GetAttribute("sale") + "\n");
+                }
+            }
+            client.SendTextMessageAsync(telegramId,tmp_currency);
+        }
+
+        public void ChangeTime(int inter)
+        {
+            timer.Interval = inter;
+        }
+
+    }
+}
---
+++ Wpf_tele_bot/Wpf_tele_bot/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Wpf_tele_bot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static TelegramBotClient client;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            client = new TelegramBotClient("1357650416:AAHwSAl43BQDVz4fCPuTvjOi8rnyTB8NNAE");
+
+            client.OnMessage += ShowCurrency;
+            client.StartReceiving();
+
+        }
+        public static void Currency(object sender, MessageEventArgs e)
+        {
+            string url = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+            XmlTextReader streamReader = new XmlTextReader(url);
+            string tmp = "";
+            while (streamReader.Read())
+            {
+                if (streamReader.HasAttributes)
+                {
+                    tmp += (streamReader.GetAttribute("ccy") + "  | " + streamReader.GetAttribute("base_ccy") + " Продажа : " + streamReader.GetAttribute("buy") + " Покупка : " + streamReader.GetAttribute("sale") + "\n");
+                }
+            }
+            client.SendTextMessageAsync(e.Message.Chat.Id, tmp);
+        }
+
+
+
+        private void getMsg(object sender, MessageEventArgs e)
+        {
+           
+
+            client.SendTextMessageAsync(e.Message.Chat.Id, "curent date & time : " + DateTime.Now.ToString());
+
+        }
+
+        public  void ShowCurrency(object sender, MessageEventArgs e)
+        {
+            string url = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
+            XmlTextReader streamReader = new XmlTextReader(url);
+            StringBuilder currensy = new StringBuilder("Currensy now : ");
+
+
+
+            switch (e.Message.Text)
+            {
+                case "Curent date & Time":
+                   client.SendTextMessageAsync(e.Message.Chat.Id, "curent date & time : " + DateTime.Now.ToString());
+                    break;
+                case "Currency":
+                    string tmp = "";
+                    while (streamReader.Read())
+                    {
+                        if (streamReader.HasAttributes)
+                        {
+                            tmp += (streamReader.GetAttribute("ccy") + "  | " + streamReader.GetAttribute("base_ccy") + " Продажа : " + streamReader.GetAttribute("buy") + " Покупка : " + streamReader.GetAttribute("sale") + "\n");
+                        }
+                        //Console.WriteLine(streamReader.GetAttribute("ccy") + " : " + streamReader.GetAttribute("base_ccy") + " : " + streamReader.GetAttribute("buy") + " : " + streamReader.GetAttribute("sale"));
+                        //client.SendTextMessageAsync(e.Message.Chat.Id, streamReader.GetAttribute("ccy") + " : " + streamReader.GetAttribute("base_ccy") + " : " + streamReader.GetAttribute("buy") + " : " + streamReader.GetAttribute("sale"));
+
+                    }
+                    client.SendTextMessageAsync(e.Message.Chat.Id, tmp);
+                    break;
+                case "Data update every 5 min":
+
+                    break;
+                case "Data update every 10 min":
+
+                    break;
+                case "btn":
+                    var markup = new ReplyKeyboardMarkup(new[] {
+                new KeyboardButton("Curent date & Time"),
+                new KeyboardButton("Currency"),
+                new KeyboardButton("Data update every 5 min"),
+                new KeyboardButton("Data update every 10 min")
+            });
+                    client.SendTextMessageAsync(e.Message.Chat.Id, "Click button", replyMarkup: markup);
+                    break;
+
+            }
+
+        }
+    }
+}
